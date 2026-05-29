a132290ea1b8b174c4eee27bc10742ea4d2ec03f
rukarajinbelajar <maar1101dnd@gmail.com>
2026-05-29 18:20:36 +0700

    menampilkan command

--- circularquequesarray.cpp
+++ circularquequesarray.cpp
@@ -54,5 +54,6 @@ public:
             cout << "Queue underflow\n";
             return;
         }
+        cout << "\nThe element deleted from the queue is: " << queue_array[FRONT] << "\n";
     }
 }
