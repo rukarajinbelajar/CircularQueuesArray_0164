aa59fec0329e19eebe9e8e15e966e2633c86c673
rukarajinbelajar <maar1101dnd@gmail.com>
2026-05-29 18:36:03 +0700

    iterasi dari awal array hingga rear

--- circularquequesarray.cpp
+++ circularquequesarray.cpp
@@ -107,5 +107,13 @@ public:
         }
 
         FRONT_Position = 0;
+
+        //iterasi dari awal array hingga rear
+        while (FRONT_Position <= REAR_Position)
+        {
+            cout << queue_array[FRONT_Position] << "  ";
+            FRONT_Position++;
+        }
+        cout << endl;
     }
 }
