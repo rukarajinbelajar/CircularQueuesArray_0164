bd8cdf231c72c785ce5780dfe2a6489aff8fa7ba
rukarajinbelajar <maar1101dnd@gmail.com>
2026-05-29 18:16:44 +0700

    untuk mengecek antrian kosong

--- circularquequesarray.cpp
+++ circularquequesarray.cpp
@@ -47,4 +47,12 @@ public:
     }
 
     void remove()
+    {
+        //cek apakah antrian kosong
+        if (FRONT == - 1)
+        {
+            cout << "Queue underflow\n";
+            return;
+        }
+    }
 }
